84dc0d12cab5b8f1060d77ea62d63c3ec4b38bb3
avaewsg <eexxxxxff@atomicmail.io>
2026-09-01 15:45:34 +0330

    Update server.js

--- server.js
+++ server.js
@@ -103,4 +103,6 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`E10 Server running on port ${PORT}`));
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
